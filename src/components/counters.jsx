6c2fd0fe99a5bf708412ffb045eda531447919c1
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-21 00:07:11 +0200

    added reset button and refactored code according to best practices

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,10 +5,18 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 2, value: 1 },
-      { id: 3, value: 2 },
-      { id: 4, value: 3 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
     ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
   };
 
   handleDelete = (counterId) => {
@@ -18,12 +26,33 @@ class Counters extends Component {
     this.render();
   };
 
+  handleReset = () => {
+    const counters = [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ];
+
+    this.setState({
+      counters: counters,
+    });
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        <br />
         {this.state.counters.map((counter) => (
           <Counter
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             key={counter.id}
             counter={counter}
           />

72610f8fb0ef305ccb0726e339780023f792e663
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-20 16:43:26 +0200

    added event handler for onClick event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 10,
+    count: 0,
     tags: ["tag1", "tag2", "tag3"],
+  };
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
   };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.renderTags()}
       </React.Fragment>
     );
